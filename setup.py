deb84b2f0332a3cab1f33a7a581de6e2cce2a70a
Kevin Morgan <kevin.d.morgan@gmail.com>
2013-06-04 17:51:10 +0100

    Making installable.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from os.path import join as pjoin
+from distutils.core import setup
+
+setup(
+    name='Codebase CLI',
+    version='0.0.1',
+    scripts=[pjoin('bin', 'cb')],
+    requires=['requests', 'gitpython', 'lxml'],
+)
