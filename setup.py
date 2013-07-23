d704f2ed7bc3ff87abadc443e20ae60e643008da
Kevin Morgan <kevin.d.morgan@gmail.com>
2013-07-23 17:33:03 +0100

    Adding additional information for PyPI.

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from os.path import join as pjoin
 from setuptools import setup
 
 setup(
-    name='Codebase CLI',
+    name='codebase-cli',
+    description='A CLI for codebasehq.com',
+    author='Kevin Morgan',
     version='0.0.1',
     scripts=[pjoin('bin', 'cb')],
     requires=[],
