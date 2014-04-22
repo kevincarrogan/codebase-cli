40cce86b174c5ed4bde5c429cb22955bc8943703
Kevin Carrogan <kevin.carrogan@gmail.com>
2014-04-22 12:27:27 +0100

    Bumping version number.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup
 setup(
     name='codebase-cli',
     description='A CLI for codebasehq.com',
-    author='Kevin Morgan',
-    author_email='kevin.d.morgan@gmail.com',
+    author='Kevin Carrogan',
+    author_email='kevin.carrogan@gmail.com',
     url='http://github.com/kevindmorgan/codebase-cli',
-    version='0.1.3',
+    version='0.1.4',
     scripts=[pjoin('bin', 'cb')],
     requires=[],
 )
